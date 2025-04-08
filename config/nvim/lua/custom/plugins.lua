e4b4e374bbfad5f7eada13cc9a8ea765b083b0af
Vasili <huzeacostas1345@gmail.com>
2025-04-08 17:20:18 +0300

    Added config

---
+++ config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,22 @@
+local plugins = {
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clangd"
+      }
+    }
+  }
+}
+return plugins
